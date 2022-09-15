6b39846019e472ead2f3fbb3cc17cd809b178b6e
Jefferson Charlles <contato@jefferdeveloper.com>
2022-09-15 08:33:42 -0300

    chore:html layout modal create

---
+++ web/src/components/FormModal/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+
+export const FormModal = () => {
+
+    return(
+        <div className={styles.container}>
+
+        </div>
+    );
+}
--- web/src/components/ModalAd/index.tsx
+++ web/src/components/ModalAd/index.tsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.css'
 import * as Dialog from '@radix-ui/react-dialog'
+import { GameController } from 'phosphor-react';
 
 export const ModalAd = () => {
 
@@ -22,6 +23,44 @@ export const ModalAd = () => {
                         <input id='name' type="text" placeholder='Como te chamam dentro do game?' />
                     </div>
 
+                    <div>
+                        <label htmlFor="name">Seu nome (ou nickname)</label>
+                        <input id='name' type="text" placeholder='Como te chamam dentro do game?' />
+                    </div>
+
+                    <div>
+                        <div>
+                            <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+                            <input id='yearsPlaying' type="text" placeholder='Tudo bem ser ZERO' />
+                        </div>
+                        <div>
+                            <label htmlFor="discord">Qual seu Discord?</label>
+                            <input id='discord' type="text" placeholder='Usuario#0000' />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <label htmlFor="weekDays">Quando costuma jogar?</label>
+                        </div>
+                        <div>
+                            <label htmlFor="hourStart">Qual horário do dia?</label>
+                            <div>
+                                <input type="time"  id="hourStart"  placeholder='De'/>
+                                <input type="time"  id="hourEnd"  placeholder='Até'/>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="" id="" />
+                        Costumo me conectar ao chat de voz
+                    </div>
+                    <footer>
+                        <button>Cancelar</button>
+                        <button type="submit">
+                            <GameController />
+                            Encontrar duo
+                            </button>
+                    </footer>
                    </form>
                 </Dialog.Content>
             </Dialog.Content>
